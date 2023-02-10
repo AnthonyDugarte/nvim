c8c9a1dd91666ae2eab8fc4eb840bb0bf7f854a2
Anthony Dugarte <toonny1998@gmail.com>
2023-02-10 17:42:40 -0400

    core: format

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -1,24 +1,25 @@
 local options = {
-  clipboard = "unnamedplus",
-	expandtab = true,
-	tabstop = 2,
-  shiftwidth = 2,
-	cursorline = true,
-	number = true,
-	relativenumber = false,
-	signcolumn = "yes",
-	wrap = false,
-	showmode = false,
-	termguicolors = true,
-	splitbelow = true,
-	splitright = true,
-  mouse = "a",
-  showtabline = 0,
+    clipboard = "unnamedplus",
+    expandtab = true,
+    tabstop = 2,
+    shiftwidth = 2,
+    cursorline = true,
+    number = true,
+    relativenumber = false,
+    signcolumn = "yes",
+    wrap = false,
+    showmode = false,
+    termguicolors = true,
+    splitbelow = true,
+    splitright = true,
+    mouse = "a",
+    showtabline = 0,
+    completeopt = { "menuone", "noselect" },
 }
 
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 for k, v in pairs(options) do
-	vim.opt[k] = v
+  vim.opt[k] = v
 end
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -1,29 +1,27 @@
 local fn = vim.fn
 
 -- Automatically install packer
-local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
-  PACKER_BOOTSTRAP = fn.system {
-    "git",
-    "clone",
-    "--depth",
-    "1",
-    "https://github.com/wbthomason/packer.nvim",
-    install_path,
-  }
-  print "Installing packer close and reopen Neovim..."
-  vim.cmd [[packadd packer.nvim]]
+  PACKER_BOOTSTRAP = fn.system({
+          "git",
+          "clone",
+          "--depth",
+          "1",
+          "https://github.com/wbthomason/packer.nvim",
+          install_path,
+      })
+  print("Installing packer close and reopen Neovim...")
+  vim.cmd([[packadd packer.nvim]])
 end
 
-
 -- Autocommand that reloads neovim whenever you save the plugins.lua file
-vim.cmd [[
+vim.cmd([[
   augroup packer_user_config
     autocmd!
     autocmd BufWritePost plugins.lua source <afile> | PackerSync
   augroup end
-]]
-
+]])
 
 -- Use a protected call so we don't error out on first use
 local status_ok, packer = pcall(require, "packer")
@@ -32,66 +30,63 @@ if not status_ok then
 end
 
 -- Have packer use a popup window
-packer.init {
-  display = {
-    open_fn = function()
-      return require("packer.util").float { border = "rounded" }
-    end,
-  },
-}
+packer.init({
+    display = {
+        open_fn = function()
+          return require("packer.util").float({ border = "rounded" })
+        end,
+    },
+})
 
 return packer.startup(function(use)
-	use "wbthomason/packer.nvim" -- Have packer manage itself
-	
-	use {
-		'nvim-tree/nvim-tree.lua',
-		requires = {
-      'nvim-tree/nvim-web-devicons',
-  	},
-  	tag = 'nightly' -- updated every week. (see issue #1193)
-	}
+      use("wbthomason/packer.nvim") -- Have packer manage itself
 
-  use {
-    'nvim-lualine/lualine.nvim',
-    requires = { 'nvim-tree/nvim-web-devicons' }
-  }
+      use({
+          "nvim-tree/nvim-tree.lua",
+          requires = { "nvim-tree/nvim-web-devicons" },
+          tag = "nightly", -- updated every week. (see issue #1193)
+      })
 
-  use 'folke/tokyonight.nvim'
+      use({
+          "nvim-lualine/lualine.nvim",
+          requires = { "nvim-tree/nvim-web-devicons" },
+      })
 
-  use {'akinsho/bufferline.nvim', tag = "v3.*", requires = 'nvim-tree/nvim-web-devicons'}
-  use "tiagovla/scope.nvim"
+      use("folke/tokyonight.nvim")
 
-  use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.1',
-    requires = { {'nvim-lua/plenary.nvim'} }
-  }
+      use({ "akinsho/bufferline.nvim", tag = "v3.*", requires = "nvim-tree/nvim-web-devicons" })
+      use("tiagovla/scope.nvim")
 
-  use {
-    'nvim-telescope/telescope-fzf-native.nvim',
-    run = 'make'
-  }
+      use({
+          "nvim-telescope/telescope.nvim",
+          tag = "0.1.1",
+          requires = { { "nvim-lua/plenary.nvim" } },
+      })
 
-  use { 
-    "akinsho/toggleterm.nvim",
-    tag = "2.3.0"
-  }
+      use({
+          "nvim-telescope/telescope-fzf-native.nvim",
+          run = "make",
+      })
 
-  -- Treesitter
-  use "nvim-treesitter/nvim-treesitter"
-  use "windwp/nvim-ts-autotag"
+      use({
+          "akinsho/toggleterm.nvim",
+          tag = "2.3.0",
+      })
 
-  -- LSP
-  use {
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
-    "neovim/nvim-lspconfig",
-    "jose-elias-alvarez/null-ls.nvim",
-    "jay-babu/mason-null-ls.nvim",
-  }
-  
+      -- Treesitter
+      use("nvim-treesitter/nvim-treesitter")
+      use("windwp/nvim-ts-autotag")
 
-  if PACKER_BOOTSTRAP then
-    require("packer").sync()
-  end
-end)
+      -- LSP
+      use({
+          "williamboman/mason.nvim",
+          "williamboman/mason-lspconfig.nvim",
+          "neovim/nvim-lspconfig",
+          "jose-elias-alvarez/null-ls.nvim",
+          "jay-babu/mason-null-ls.nvim",
+      })
 
+      if PACKER_BOOTSTRAP then
+        require("packer").sync()
+      end
+    end)
