b1206040820738ae50d2daeb961f1becd9aa5b73
Anthony Dugarte <toonny1998@gmail.com>
2023-02-12 11:03:11 -0400

    fix: Unmap tab navigation, works buggy

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -15,18 +15,16 @@ keymap("n", "<C-i>", "<C-i>", opts)
 keymap("n", "<leader>e", ":NvimTreeFindFileToggle<cr>", opts)
 
 -- Tabs (works weird when closing buffers)
-keymap("n", "<enter>", ":tabnew %<cr>", opts)
-keymap("n", "<s-enter>", ":tabclose<cr>", opts)
-keymap("n", "<m-\\>", ":tabonly<cr>", opts)
--- Buffers
-keymap("n", "<leader>bd", ":bd<CR>", opts)
+-- keymap("n", "<enter>", ":tabnew %<cr>", opts)
+-- keymap("n", "<s-enter>", ":tabclose<cr>", opts)
+-- keymap("n", "<m-\\>", ":tabonly<cr>", opts)
+-- keymap("n", "<s-j>", ":tabp<CR>", opts)
+-- keymap("n", "<s-k>", ":tabn<CR>", opts)
 
--- Navigate tabs
-keymap("n", "<s-j>", ":tabp<CR>", opts)
-keymap("n", "<s-k>", ":tabn<CR>", opts)
--- Naviagate buffers
+-- Buffers
 keymap("n", "<s-h>", ":bp<CR>", opts)
 keymap("n", "<s-l>", ":bn<CR>", opts)
+keymap("n", "<leader>bd", ":bd<CR>", opts)
 
 -- Windows movement
 keymap("n", "<C-h>", "<C-w>h", opts)
@@ -38,6 +36,8 @@ keymap("t", "<C-h>", "<cmd>wincmd h<CR>", term_opts)
 keymap("t", "<C-j>", "<cmd>wincmd j<CR>", term_opts)
 keymap("t", "<C-k>", "<cmd>wincmd k<CR>", term_opts)
 keymap("t", "<C-l>", "<cmd>wincmd l<CR>", term_opts)
+-- Interations
+keymap("n", "<C-|>", ":ToggleTermToggleAll<CR>", opts)
 
 -- Visual
 -- Keep visual mode on identation
@@ -45,13 +45,9 @@ keymap("v", "<", "<gv", opts)
 keymap("v", ">", ">gv", opts)
 
 -- Telescope
-keymap('n', '<leader>ff', ":Telescope find_files<CR>", opts)
-keymap('n', '<leader>fg', ":Telescope live_grep<CR>", opts)
-keymap('n', '<leader>fb', ":Telescope buffers<CR>", opts)
-keymap('n', '<leader>fh', ":Telescope help_tags<CR>", opts)
+keymap("n", "<leader>ff", ":Telescope find_files<CR>", opts)
+keymap("n", "<leader>fg", ":Telescope live_grep<CR>", opts)
+keymap("n", "<leader>fb", ":Telescope buffers<CR>", opts)
+keymap("n", "<leader>fh", ":Telescope help_tags<CR>", opts)
 -- Telescope LSP handling
-keymap('n', '<leader>fr', ":Telescope lsp_references<CR>", opts)
-
-
--- Terminal interations
-keymap('n', '<C-|>', ":ToggleTermToggleAll<CR>", opts)
+keymap("n", "<leader>fr", ":Telescope lsp_references<CR>", opts)
