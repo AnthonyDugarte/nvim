0a1cd4a43c6286dfaf4c8a6c23bf36703d2f9109
Anthony Dugarte <toonny1998@gmail.com>
2023-02-13 17:56:53 -0400

    feat(toggleterm): Add lazygit terminal via <leader>g

--- lua/user/toggleterm.lua
+++ lua/user/toggleterm.lua
@@ -1,12 +1,34 @@
 local status_ok, toggleterm = pcall(require, "toggleterm")
 
 if not status_ok then
-  return
+	return
 end
 
-toggleterm.setup {
-  size = 20,
-  open_mapping = [[<c-\>]],
-  insert_mappings = true,
-  terminal_mappings = false,
-}
+toggleterm.setup({
+	size = 20,
+	open_mapping = [[<c-\>]],
+	insert_mappings = true,
+	terminal_mappings = false,
+})
+
+local Terminal = require("toggleterm.terminal").Terminal
+local lazygit = Terminal:new({
+	cmd = "lazygit",
+	hidden = true,
+	direction = "tab",
+	on_open = function(_)
+		vim.cmd("startinsert!")
+		vim.cmd("set laststatus=0")
+	end,
+	on_close = function(_)
+		vim.cmd("set laststatus=3")
+	end,
+	count = 99,
+})
+
+function _lazygit_toggle()
+	lazygit:toggle()
+end
+
+vim.api.nvim_set_keymap("n", "<leader>g", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
+
