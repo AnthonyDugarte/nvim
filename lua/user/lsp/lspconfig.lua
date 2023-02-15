cd8387d72bbb9f6810ff61aa2dfb111fee6b880e
Anthony Dugarte <toonny1998@gmail.com>
2023-02-14 23:07:32 -0400

    fix(lsp): add keymap alias

--- lua/user/lsp/lspconfig.lua
+++ lua/user/lsp/lspconfig.lua
@@ -19,25 +19,28 @@ if not status_ok then
 	return
 end
 
--- Attach keymaps, taken from: https://github.com/neovim/nvim-lspconfig#suggested-configuration
+local keymap = vim.api.nvim_set_keymap
+
 local on_attach = function(client, bufnr)
 	local bufopts = { noremap = true, silent = true, buffer = bufnr }
-	vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
-	vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
-	vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
-	vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
-	vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
-	vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, bufopts)
-	vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, bufopts)
-	vim.keymap.set("n", "<space>wl", function()
+
+	keymap("n", "gD", vim.lsp.buf.declaration, bufopts)
+	keymap("n", "gd", vim.lsp.buf.definition, bufopts)
+	keymap("n", "K", vim.lsp.buf.hover, bufopts)
+	keymap("n", "gi", vim.lsp.buf.implementation, bufopts)
+	keymap("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
+	keymap("n", "<space>wa", vim.lsp.buf.add_workspace_folder, bufopts)
+	keymap("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, bufopts)
+	keymap("n", "<space>wl", function()
 		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
 	end, bufopts)
-	vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, bufopts)
-	vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, bufopts)
-	vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, bufopts)
+	keymap("n", "<space>D", vim.lsp.buf.type_definition, bufopts)
+	keymap("n", "<space>rn", vim.lsp.buf.rename, bufopts)
+	keymap("n", "<space>ca", vim.lsp.buf.code_action, bufopts)
 
-	vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
-	vim.keymap.set("n", "<space>f", function()
+	keymap("n", "gr", vim.lsp.buf.references, bufopts)
+
+	keymap("n", "<space>f", function()
 		vim.lsp.buf.format({ async = true })
 	end, bufopts)
 end
