d7d9fb7d70e36c03ad23d6e889134170833d77da
Anthony Dugarte <toonny1998@gmail.com>
2023-05-10 10:19:33 -0400

    feat(nvim-tree): Add float tree view

--- lua/user/nvim-tree/init.lua
+++ lua/user/nvim-tree/init.lua
@@ -13,6 +13,9 @@ api.events.subscribe(api.events.Event.FileCreated, function(file)
 	vim.cmd("edit " .. file.fname)
 end)
 
+local HEIGHT_RATIO = 0.8
+local WIDTH_RATIO = 0.5
+
 nvim_tree.setup({
 	filters = {
 		custom = { ".git$" },
@@ -22,12 +25,39 @@ nvim_tree.setup({
 		ignore = false,
 	},
 	sync_root_with_cwd = true,
+	-- view = {
+	-- 	width = {
+	-- 		min = 30,
+	-- 		max = 300,
+	-- 	},
+	-- 	-- side = "right",
+	-- },
+	--
 	view = {
-		width = {
-			min = 30,
-			max = 300,
+		float = {
+			enable = true,
+			open_win_config = function()
+				local screen_w = vim.opt.columns:get()
+				local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+				local window_w = screen_w * WIDTH_RATIO
+				local window_h = screen_h * HEIGHT_RATIO
+				local window_w_int = math.floor(window_w)
+				local window_h_int = math.floor(window_h)
+				local center_x = (screen_w - window_w) / 2
+				local center_y = ((vim.opt.lines:get() - window_h) / 2) - vim.opt.cmdheight:get()
+				return {
+					border = "rounded",
+					relative = "editor",
+					row = center_y,
+					col = center_x,
+					width = window_w_int,
+					height = window_h_int,
+				}
+			end,
 		},
-		-- side = "right",
+		width = function()
+			return math.floor(vim.opt.columns:get() * WIDTH_RATIO)
+		end,
 	},
 	actions = {
 		open_file = {
