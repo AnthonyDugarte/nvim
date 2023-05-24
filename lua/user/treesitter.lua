13e3a703b3579d0523fb920827e2b26665aed20d
Anthony Dugarte <toonny1998@gmail.com>
2023-05-23 21:01:05 -0400

    fix(ts): selection

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -20,4 +20,13 @@ treesitter.setup({
 		enable = true,
 		enable_autocmd = false,
 	},
+	incremental_selection = {
+		enable = true,
+		keymaps = {
+			init_selection = "gnn",
+			node_incremental = "grn",
+			scope_incremental = "grc",
+			node_decremental = "grm",
+		},
+	},
 })
