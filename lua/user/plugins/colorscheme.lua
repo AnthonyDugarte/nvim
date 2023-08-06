6cc7b1d7ff3ffbc9352c76fea3dae23d7b2f881f
Anthony Dugarte <toonny1998@gmail.com>
2023-08-06 17:03:33 -0400

    feat: versions

--- lua/user/plugins/colorscheme.lua
+++ lua/user/plugins/colorscheme.lua
@@ -3,48 +3,7 @@ return {
 		"catppuccin/nvim",
 		name = "catppuccin",
 		priority = 1000,
-		enabled = false,
-		opts = {
-			dim_inactive = {
-				enabled = false,
-				shade = "dark",
-				percentage = 0.15,
-			},
-			integrations = {
-				bufferline = true,
-				cmp = true,
-				fidget = true,
-				gitsigns = true,
-				lsp_trouble = true,
-				indent_blankline = {
-					enabled = true,
-					colored_indent_levels = false,
-				},
-				mason = true,
-				native_lsp = {
-					enabled = true,
-					virtual_text = {
-						errors = { "italic" },
-						hints = { "italic" },
-						warnings = { "italic" },
-						information = { "italic" },
-					},
-					underlines = {
-						errors = { "underline" },
-						hints = { "underline" },
-						warnings = { "underline" },
-						information = { "underline" },
-					},
-					inlay_hints = {
-						background = true,
-					},
-				},
-				nvimtree = true,
-				telescope = true,
-				treesitter = true,
-				which_key = true,
-			},
-		},
+		enabled = true,
 		config = function()
 			vim.cmd([[colorscheme catppuccin-frappe]])
 		end,
@@ -53,7 +12,7 @@ return {
 		"folke/tokyonight.nvim",
 		lazy = false,
 		priority = 1000,
-		enabled = true,
+		enabled = false,
 		config = function()
 			vim.cmd([[colorscheme tokyonight]])
 		end,
